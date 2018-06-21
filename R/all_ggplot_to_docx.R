6521b2387dc99e56b5c447d3d2df0d761da6900d
Vincent Guyader <vincent@thinkr.fr>
2018-06-21 06:41:47 +0000

    version 0.13

--- R/all_ggplot_to_docx.R
+++ R/all_ggplot_to_docx.R
@@ -6,13 +6,14 @@
 #' @param global booleen use .GlobalEnv
 #' @param folder png's folder
 #'
-#' @importFrom  ReporteRs pptx addSlide addPlot writeDoc
+#' @importFrom  officer read_pptx ph_with_gg add_slide
 #' @import ggplot2
 #' @import assertthat
 #' @return NULL
 #' @encoding UTF-8
 #' @export
 #' @importFrom utils browseURL
+#' @importFrom rvg ph_with_vg
 #'
 #' @examples
 #' \dontrun{
@@ -20,8 +21,8 @@
 all_ggplot_to_pptx <- function(out="tous_les_graphs.pptx",open=TRUE,png=TRUE,folder="dessin",global=TRUE){
   assertthat::assert_that(has_extension(out,"pptx"))
   try(dir.create(folder,recursive = TRUE),silent=TRUE)
-  doc <- pptx( title = "title" )
-  dim(doc)
+  # doc <- pptx( title = "title" )
+  doc <- read_pptx()
   if (global){
     lenv<- .GlobalEnv}else{
       lenv<- parent.frame()
@@ -35,14 +36,20 @@ all_ggplot_to_pptx <- function(out="tous_les_graphs.pptx",open=TRUE,png=TRUE,fol
 
 
 
-    if(is.ggplot(eval(envir = lenv,parse(text=k)))){
-      doc <- addSlide( doc, slide.layout = "Title and Content" )
-      doc <- addPlot( doc, function( ) print( eval(envir = lenv,parse(text=k)) ),
-                      offx =0    ,offy=0,
-                      height = dim(doc)$slide.dim[["height"]],width = dim(doc)$slide.dim[["width"]],
-                      vector.graphic = TRUE, editable = TRUE )
+    if (is.ggplot(
+      # eval(envir = lenv,parse(text=k))
+      get(k,envir = lenv)
+      )){
+      # doc <- addSlide( doc, slide.layout = "Title and Content" )
+      doc <- doc %>%
+        add_slide(layout = "Title and Content", master = "Office Theme") %>%
+        ph_with_vg(ggobj = get(k,envir = lenv),type = "body" )
+
       if(png){
-        ggsave(eval(envir = lenv,parse(text=k)),filename = paste0(folder,"/",k,".png"),height=15,width=23)
+        ggsave(
+          # eval(envir = lenv,parse(text=k))
+          get(k,envir = lenv)
+          ,filename = paste0(folder,"/",k,".png"),height=15,width=23)
 
       }
     }
@@ -50,12 +57,13 @@ all_ggplot_to_pptx <- function(out="tous_les_graphs.pptx",open=TRUE,png=TRUE,fol
 
     # on gere les ggsurvfit
 
-    if(class(eval(envir = lenv,parse(text=k)))[1]=="ggsurvplot"){
-      doc <- addSlide( doc, slide.layout = "Title and Content" )
-      doc <- addPlot( doc, function( ) print( eval(envir = lenv,parse(text=k))$plot ),
-                      offx =0    ,offy=0,
-                      height = dim(doc)$slide.dim[["height"]],width = dim(doc)$slide.dim[["width"]],
-                      vector.graphic = TRUE, editable = TRUE )
+    if(class(get(k,envir = lenv))[1]=="ggsurvplot"){
+      doc <- doc %>%
+        add_slide(layout = "Title and Content", master = "Office Theme") %>%
+        ph_with_vg( ggobj = get(k,envir = lenv)$plot )
+
+
+
       if(png){
         ggsave(eval(envir = lenv,parse(text=k))$plot,filename = paste0(folder,"/",k,".png"),height=15,width=23)
 
@@ -68,8 +76,7 @@ all_ggplot_to_pptx <- function(out="tous_les_graphs.pptx",open=TRUE,png=TRUE,fol
 
 
   }
-
-  ReporteRs::writeDoc(doc,file=out)
+  print(doc, target = out )
   if (open){browseURL(out)}
   invisible()
 }
